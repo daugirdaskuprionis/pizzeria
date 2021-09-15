09e2892440805525fed960bb7dd88af2d27c7c43
Daugirdas <daugirdaskuprioni@outlook.com>
2021-09-15 14:22:55 -0700

    sorting function added

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 let menu = [];
+let sortValue = "Name";
 
-const displayMenu = function(arr) {
-    let displayItem = "";
-    let chilliImg = '<img class="chilli" src="./chillipepper.png">';
-
-    if (arr == null || arr == "" ) {
-        return "";
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-        displayItem +=
-        `
-        <li>
-            <div class="pizza">
-                <img src="${menu[i].photo}.jpg">
-                <span class="pizzaDesc">
-                    <p><b>Name:</b> ${menu[i].name} <span>${chilliImg.repeat(menu[i].heat)}</span></p>
-                    <p><b>Price:</b> ${menu[i].price}</p>
-                    <p><b>Toppings:</b> ${menu[i].toppings}</p>
-                </span>
-                <button onclick="deleteItem(${i})" class="buttonDelete">Delete</button>
-            </div>
-        </li>
-        `
-    }
-    return displayItem;
-}
+const sortByName = function(a, b) { menu.sort((a, b) => a.name.localeCompare(b.name)) }
+const sortByPrice = function(a, b) { menu.sort((a, b) => a.price - b.price); }
+const sortByHeat = function(a, b) { menu.sort((a, b) => a.heat - b.heat); }
 
 const checkName = function(name) {
     for (let i = 0; i < menu.length; i++) {
@@ -40,7 +18,7 @@ const deleteItem = function(item) {
     if (confirm("Are you sure?")) {
         menu.splice(item, 1);
         sessionStorage.setItem('pizza', JSON.stringify(menu));
-        document.querySelector('.displayMenu').innerHTML = displayMenu(menu);
+        document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
     }
 }
 
@@ -52,6 +30,38 @@ const toppings = function() {
     }
     return toppings.join(', ');
 }
+
+const displayMenu = function(sortValue, arr) {
+    let displayItem = "";
+    let chilliImg = '<img class="chilli" src="./chillipepper.png">';
+
+    if (sortValue == 'Name') { sortByName(); }
+    if (sortValue == 'Price') { sortByPrice(); }
+    if (sortValue == 'Heat') { sortByHeat(); }
+
+    for (let i = 0; i < arr.length; i++) {
+        displayItem +=
+        `
+        <li class="pizza">
+            <img src="${menu[i].photo}.jpg">
+            <span class="pizzaDesc">
+                <p><b>Name:</b> ${menu[i].name} <span>${chilliImg.repeat(menu[i].heat)}</span></p>
+                <p><b>Price:</b> ${menu[i].price}</p>
+                <p><b>Toppings:</b> ${menu[i].toppings}</p>
+            </span>
+            <button onclick="deleteItem(${i})" class="buttonDelete">Delete</button>
+        </li>
+        `
+    }
+    return displayItem;
+}
+
+document.querySelector('.sortButton').addEventListener('click', function() {
+    const sortBy = document.querySelector('.sortBySelection');
+    let value = sortBy.options[sortBy.selectedIndex].value;
+    sortValue = value;
+    document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
+});
 
 document.querySelector('.toppingButton').addEventListener('click', function(e) {
     e.preventDefault();
@@ -65,9 +75,7 @@ document.querySelector('.toppingButton').addEventListener('click', function(e) {
 document.querySelector('.pizzaForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if (checkName(document.getElementById('name').value) == false) {
-        return false;
-    }
+    if (checkName(document.getElementById('name').value) == false) { return false; }
 
     pizza = {
         name: document.getElementById('name').value,
@@ -79,9 +87,9 @@ document.querySelector('.pizzaForm').addEventListener('submit', function(e) {
 
     menu.push(pizza);
     sessionStorage.setItem('pizza', JSON.stringify(menu));
-    document.querySelector('.displayMenu').innerHTML = displayMenu(menu);
+    document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
     document.querySelector('.pizzaForm').reset();
 });
 
 menu = JSON.parse(sessionStorage.getItem("pizza")) || [];
-document.querySelector('.displayMenu').innerHTML = displayMenu(menu);
+document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
