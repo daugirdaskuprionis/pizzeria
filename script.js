271a9574be3e1dff35132e1bde767c3adce0bad9
Daugirdas <daugirdaskuprioni@outlook.com>
2021-09-15 14:40:44 -0700

    name null validation

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 let menu = [];
 let sortValue = "Name";
 
-const sortByName = function(a, b) { menu.sort((a, b) => a.name.localeCompare(b.name)) }
-const sortByPrice = function(a, b) { menu.sort((a, b) => a.price - b.price); }
-const sortByHeat = function(a, b) { menu.sort((a, b) => a.heat - b.heat); }
-
 const checkName = function(name) {
     for (let i = 0; i < menu.length; i++) {
         if (name.toLowerCase() == menu[i].name.toLowerCase()) {
@@ -56,6 +52,10 @@ const displayMenu = function(sortValue, arr) {
     return displayItem;
 }
 
+const sortByName = function(a, b) { menu.sort((a, b) => a.name.localeCompare(b.name)) }
+const sortByPrice = function(a, b) { menu.sort((a, b) => a.price - b.price); }
+const sortByHeat = function(a, b) { menu.sort((a, b) => a.heat - b.heat); }
+
 document.querySelector('.sortButton').addEventListener('click', function() {
     const sortBy = document.querySelector('.sortBySelection');
     let value = sortBy.options[sortBy.selectedIndex].value;
@@ -63,19 +63,11 @@ document.querySelector('.sortButton').addEventListener('click', function() {
     document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
 });
 
-document.querySelector('.toppingButton').addEventListener('click', function(e) {
-    e.preventDefault();
-    let inputEl = document.createElement('input');
-    inputEl.toppings = 'toppings';
-    inputEl.type = 'text';
-    inputEl.name = 'toppings[]';
-    document.querySelector('.toppingsLabel').appendChild(inputEl);
-});
-
 document.querySelector('.pizzaForm').addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (checkName(document.getElementById('name').value) == false) { return false; }
+    if (document.getElementById('name').length == null) { alert("Name field must have value."); return false; }
 
     pizza = {
         name: document.getElementById('name').value,
@@ -89,6 +81,15 @@ document.querySelector('.pizzaForm').addEventListener('submit', function(e) {
     sessionStorage.setItem('pizza', JSON.stringify(menu));
     document.querySelector('.displayMenu').innerHTML = displayMenu(sortValue, menu);
     document.querySelector('.pizzaForm').reset();
+});
+
+document.querySelector('.toppingButton').addEventListener('click', function(e) {
+    e.preventDefault();
+    let inputEl = document.createElement('input');
+    inputEl.toppings = 'toppings';
+    inputEl.type = 'text';
+    inputEl.name = 'toppings[]';
+    document.querySelector('.toppingsLabel').appendChild(inputEl);
 });
 
 menu = JSON.parse(sessionStorage.getItem("pizza")) || [];
